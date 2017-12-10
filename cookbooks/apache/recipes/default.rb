13a85f4a2538069be2a5f6fe62aea5524a7e4b98
Sultan <sultanmirza602@gmail.com>
2017-12-10 19:08:04 +0000

    Adding Chef-Repo to git

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,22 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-
-if node ['platform_family'] == "rhel"
-	package = "httpd"
-elsif node ['platform_family'] == "debian"
-	package = "apache2"
-end
-
+#
 package 'apache2' do
-	package_name package
+	package_name 'httpd'
 	action :install
 end
 
 service 'apache2' do
-	service_name package 
-	action [:restart, :enable]
+	service_name 'httpd'
+	action [:start, :enable]
 end
-
-
 
